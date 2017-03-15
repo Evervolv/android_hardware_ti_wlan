9bb6e4deb684f7153a34b33b9864d0a00339944f
D. Andrei Măceș <dmaces@nd.edu>
2017-03-15 10:14:38 -0400

    wl12xx: Add SR/MR/PLT FW for R8 (Rev. 6.3.10.0.133/6.5.7.0.42)
    While wl12[78]x-fw-5-{sr,mr,plt}.bin modules are available, they're
    not linked in wl12[78]x-wlan-products.mk to prevent including them
    with compat_wl12xx-based installations. This R8 FW is not strictly
    needed with modified compat_wl18xx drivers and fw-4 might be newer.
    
    Change-Id: Ib4c0ddd23c9b155c7e988baf62f5bfb7ece302b1

--- mac80211/firmware/128x/Android.mk
+++ mac80211/firmware/128x/Android.mk
@@ -26,6 +26,30 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := wl128x-fw-5-sr.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wl128x-fw-5-mr.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wl128x-fw-5-plt.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := wl1271-nvs_128x.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(local_target_dir)
